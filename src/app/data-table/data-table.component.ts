7fcd593ed305a3fdeb9e52999c7f79e07db9314a
lizann29 <lizininua2001@gmail.com>
2023-09-08 16:19:53 +0400

    add pagination

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,21 +9,40 @@ import { HttpClient } from '@angular/common/http';
 export class DataTableComponent implements OnInit {
   carsData: any[] = [];
   filterText: string = '';
-  errorMessage: string = '';
-  constructor(private http: HttpClient) {
-    }
 
-ngOnInit(): void {
-  this.http.get<any[]>('https://localhost:7028/api/Car/GetAllCars?username=mygpscars&password=mygpscars12345').subscribe(
+  currentPage: number = 1;
+  totalPages: any; 
+  itemsPerPage: number = 15; 
+  pageindex: number = 1;
+  
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://localhost:7028/api/Car/GetAllCars?username=mygpscars&password=mygpscars12345').subscribe(
       (data) => {
         this.carsData = data;
+        this.totalPages = Math.ceil(data.length / this.itemsPerPage); 
+    
         console.log(this.carsData);
       },
       (error) => {
-        this.errorMessage = 'An error occurred while fetching car data. Please try again later.';
-        console.error('Error loading cars data by dates:', error);
+        console.error('Error loading cars', error);
       }
     );
- }
- 
+    
+  }
+
+  previousMainPage() {
+    if (this.pageindex > 1) {
+      this.pageindex--;
+    }
+  }
+  
+  nextMainPage() {
+    if (this.pageindex < this.totalPages) {
+      this.pageindex++;
+    }
+  }
+  
 }
